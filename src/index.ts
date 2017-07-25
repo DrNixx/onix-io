6c9c86c400fef6558a32dcd2d92f4f8344a8ad8f
Nick Panteleeff <drnix@chess-online.com>
2017-07-25 18:06:48 +0300

    Publish

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { PushStream} from './net/PushStream';
 export { LatencyResolver} from './net/LatencyResolver';
+export { IError, ConnectionStatus, MessageType, IServerMessage, IServerAdapter } from './net/Types';
+export { onixPostMessage, onixReceiveMessage } from './net/PostMessage';
+export { EventSourceSubjectConfig, EventSourceSubject } from './net/EventSourceSubject';
